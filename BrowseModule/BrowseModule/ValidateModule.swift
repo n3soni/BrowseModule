c64f4d886d4234b9a881188caf3015d8b488e7b3
Nitin Soni <nitin.soni4@t-mobile.com>
2021-04-09 01:15:08 +0530

    Updated reply message

--- BrowseModule/BrowseModule/ValidateModule.swift
+++ BrowseModule/BrowseModule/ValidateModule.swift
@@ -9,6 +9,6 @@ import Foundation
 
 public struct Validator {
     public static func whereAreYou() {
-        print("I am inside BrowseModule")
+        print("Hi there, I am inside BrowseModule")
     }
 }
