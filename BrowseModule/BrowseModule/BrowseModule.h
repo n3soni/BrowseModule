2ec413441d44403176f23b9a689605cf52cd1472
Nitin Soni <nitin.soni4@t-mobile.com>
2021-04-08 23:16:01 +0530

    added base code for module

---
+++ BrowseModule/BrowseModule/BrowseModule.h
@@ -0,0 +1,18 @@
+//
+//  BrowseModule.h
+//  BrowseModule
+//
+//  Created by Nitin Soni on 08/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BrowseModule.
+FOUNDATION_EXPORT double BrowseModuleVersionNumber;
+
+//! Project version string for BrowseModule.
+FOUNDATION_EXPORT const unsigned char BrowseModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BrowseModule/PublicHeader.h>
+
+
---
+++ BrowseModule/BrowseModule/ValidateModule.swift
@@ -0,0 +1,14 @@
+//
+//  ValidateModule.swift
+//  BrowseModule
+//
+//  Created by Nitin Soni on 08/04/21.
+//
+
+import Foundation
+
+public struct Validator {
+    public static func whereAreYou() {
+        print("I am inside BrowseModule")
+    }
+}
